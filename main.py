53dac4ff656807ffbd7c425c8510a347e697e2de
xsonykkx <sofiya.strologo@gmail.com>
2024-07-17 17:32:20 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ primes = []
 not_primes = []
 
 for i in range(2, len(numbers) + 1):
-    is_prime = True
-    for j in range(2, i):
-        if i % j == 0:
-            is_prime = False
-            break
-    primes.append(i)
-else:
-    not_primes.append(i)
+        is_prime = True
+        for j in range(2, i):
+            if i % j == 0:
+                is_prime = False
+                break
+        if is_prime:
+         primes.append(i)
+        else:
+         not_primes.append(i)
 
 
 print('Primes:', primes)
